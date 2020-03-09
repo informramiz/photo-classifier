0e160a4aaa8c5041bfa57517436c3de85de98506
Ramiz Raja <informramiz@gmail.com>
2020-03-09 23:55:39 +0500

    chore: Create project

---
+++ PhotoClassifier/PhotoClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoClassifier
+//
+//  Created by Ramiz Raja on 09/03/2020.
+//  Copyright © 2020 RR Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
